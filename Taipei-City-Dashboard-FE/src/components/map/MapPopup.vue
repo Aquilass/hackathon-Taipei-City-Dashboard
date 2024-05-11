bb152319f57f26363736091b902aeba0b46a488e
janlin002 <123davidbill@gmail.com>
2024-05-12 07:06:25 +0800

    feat: hackathon

--- Taipei-City-Dashboard-FE/src/components/map/MapPopup.vue
+++ Taipei-City-Dashboard-FE/src/components/map/MapPopup.vue
@@ -4,40 +4,44 @@
 <script setup></script>
 
 <template>
-  <div class="mappopup">
-    <div class="mappopup-tab">
-      <div
-        v-for="(mapConfig, index) in mapConfigs"
-        :key="mapConfig.id"
-        :class="{ 'mappopup-tab-active': activeTab === index }"
-      >
-        <button
-          @click="
-            () => {
-              activeTab = index;
-            }
-          "
-        >
-          {{
-            activeTab === index
-              ? mapConfig.title
-              : mapConfig.title.length > 5
-                ? mapConfig.title.slice(0, 4) + "..."
-                : mapConfig.title
-          }}
-        </button>
-      </div>
-    </div>
-    <div class="mappopup-content">
-      <div
-        v-for="item in mapConfigs[activeTab].property"
-        :key="item.key"
-      >
-        <h3>{{ item.name }}</h3>
-        <p>{{ popupContent[activeTab].properties[item.key] }}</p>
-      </div>
-    </div>
-  </div>
+	<div class="mappopup">
+		<div class="mappopup-tab">
+			<div
+				v-for="(mapConfig, index) in mapConfigs"
+				:key="mapConfig.id"
+				:class="{ 'mappopup-tab-active': activeTab === index }"
+			>
+				<button
+					@click="
+						() => {
+							activeTab = index;
+						}
+					"
+				>
+					{{
+						activeTab === index
+							? mapConfig.title
+							: mapConfig.title.length > 5
+							? mapConfig.title.slice(0, 4) + "..."
+							: mapConfig.title
+					}}
+				</button>
+			</div>
+		</div>
+		<div class="mappopup-content">
+			<div v-for="item in mapConfigs[activeTab].property" :key="item.key">
+				<h3 v-if="item.key !== '攝影機路徑'">{{ item.name }}</h3>
+				<div :style="{ display: 'inline-block' }">
+					<p v-if="item.key !== '攝影機路徑'">
+						{{ popupContent[activeTab].properties[item.key] }}
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="mappopup-content">
+			<img :src="url" width="100%" height="100%" />
+		</div>
+	</div>
 </template>
 
 <style lang="scss">
@@ -53,7 +57,7 @@
 
 .mapboxgl-popup {
 	width: fit-content;
-	min-width: 310px !important;
+	min-width: 310px;
 	animation: easein 0.2s linear;
 }
 
@@ -103,6 +107,11 @@
 		color: var(--color-complement-text);
 	}
 
+	&-custom {
+		height: 100%;
+		padding: 10px;
+	}
+
 	&-tab {
 		display: flex;
 		margin-bottom: 0.5rem;
@@ -116,7 +125,9 @@
 			color: var(--color-complement-text);
 			font-size: var(--font-s);
 			text-align: center;
-			transition: color 0.2s, opacity 0.2s;
+			transition:
+				color 0.2s,
+				opacity 0.2s;
 			user-select: none;
 
 			&:hover {
